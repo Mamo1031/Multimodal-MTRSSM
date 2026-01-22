13e47153d0d9e42cfd87577b5489c7f8aeea3d54
Mamo1031 <mamoru_ota1031@keio.jp>
2026-01-22 11:13:19 +0900

    feat: Create common models package for RSSM variants, consolidating data modules, networks, objectives, and transformations

---
+++ src/multimodal_rssm/models/__init__.py
@@ -0,0 +1,52 @@
+"""Common models package for RSSM variants."""
+
+from multimodal_rssm.models.callback import WandBMetricOrganizer
+from multimodal_rssm.models.dataset import (
+    BaseEpisodeDataModule,
+    BaseEpisodeDataModuleConfig,
+    EpisodeDataset,
+    Transform,
+    convert_gdrive_url,
+    load_tensor,
+    split_path_list,
+)
+from multimodal_rssm.models.networks import Representation, Transition
+from multimodal_rssm.models.objective import likelihood
+from multimodal_rssm.models.state import State, cat_states, stack_states
+from multimodal_rssm.models.transform import (
+    GaussianNoise,
+    NormalizeAction,
+    NormalizeAudioMelSpectrogram,
+    NormalizeVisionImage,
+    RandomWindow,
+    RemoveDim,
+    RemoveHead,
+    RemoveTail,
+    TakeFirstN,
+)
+
+__all__ = [
+    "BaseEpisodeDataModule",
+    "BaseEpisodeDataModuleConfig",
+    "EpisodeDataset",
+    "GaussianNoise",
+    "NormalizeAction",
+    "NormalizeAudioMelSpectrogram",
+    "NormalizeVisionImage",
+    "RandomWindow",
+    "RemoveDim",
+    "RemoveHead",
+    "RemoveTail",
+    "Representation",
+    "State",
+    "TakeFirstN",
+    "Transform",
+    "Transition",
+    "WandBMetricOrganizer",
+    "cat_states",
+    "convert_gdrive_url",
+    "likelihood",
+    "load_tensor",
+    "split_path_list",
+    "stack_states",
+]
